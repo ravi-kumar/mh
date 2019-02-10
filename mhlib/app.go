aa9168b30559327dccadee037dc3c86e2fad8f19
Ravi Kumar <raviku7@cisco.com>
2019-02-10 02:42:21 -0800

    Add noRecreatePods commandline flag

--- mhlib/app.go
+++ mhlib/app.go
@@ -165,12 +165,8 @@ func (a *App) apply(configFile string, simulate bool) (*[]interface{}, error) {
 	// Prepare to do `helm upgrade`
 	cmd := []interface{}{"upgrade", a.ID, *chart}
 
-	// Always be verbose for debugging (because 'a.PrintRendered' doesn't work for 'apply')
-	cmd = append(cmd, "--debug")
-
 	if a.PrintRendered {
 		// "enable verbose output"
-		cmd = append(cmd, "--debug")
 		fmt.Print(string(*overrides))
 	}
 
@@ -193,8 +189,10 @@ func (a *App) apply(configFile string, simulate bool) (*[]interface{}, error) {
 	// "if a release by this name doesn't already exist, run an install"
 	cmd = append(cmd, "--install")
 
-	// "performs pods restart for the resource if applicable"
-	cmd = append(cmd, "--recreate-pods")
+	if !a.NoRecreatePods {
+		// "performs pods restart for the resource if applicable"
+		cmd = append(cmd, "--recreate-pods")
+	}
 
 	// "namespace to install the release into. (Defaults to helm default behaviour => kubeconfig checked out ns)"
 	if a.Namespace != "" {
